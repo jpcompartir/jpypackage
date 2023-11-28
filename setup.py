9363e70d8873c8a75ea930ba721f02e8577d231b
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-11-28 10:06:54 +0000

    initialise and add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "jpypackage",
+    version = "0.1",
+    packages = find_packages(),
+    description = "Utility functions and miscellaneous code chunks for Python workflows and work-related stuff."
+)
