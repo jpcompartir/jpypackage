4ebf6b9837c388a2345c6418c415fcf4d0d3fae2
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-11-28 11:44:43 +0000

    update requirements.txt and add the base for openai prompt models

---
+++ jpypackage/prompts.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Optional, Dict
+
+class Prompt(BaseModel):
+    name: str
+    description: str
+    system: str
+    user: str
+    model: str
+    response_format: Optional[Dict] = {"type": "json_object"}
+    temperature: Optional[float] = 0.0
+    
+absa = Prompt(
+    name = "Aspect-based sentiment analysis",
+    description = "Prompt for identifying aspects in a document and extracting a sentiment classification for each aspect,",
+    system = "You are a helpful assistant designed to output JSON. You identify companies, products, and services in a document and extracting a sentiment classification for each aspect.\nOutput JSON  fields for entity, aspect, sentiment.",
+    user = "",
+    model = "gpt-4-1106-preview"
+)
+
+entity_sentiment = Prompt(
+    name = "An entity-specific sentiment classification",
+    description = "Prompt for identifying high-level entities in a document and extracting a sentiment classification for each entity",
+    system = "You are a helpful assistant designed to output JSON. You recognise Named Entities and output a sentiment classification for each entity.\nOutput JSON fields for entity, and sentiment only.",
+    user = "",
+    model = "gpt-3.5-turbo-1106"
+)
